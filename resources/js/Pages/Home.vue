a51c8ea615376228f93fd0126ea02fcefa462a5c
Elhya13 <aureliewinkin@gmail.com>
2023-01-18 16:12:51 +0100

    rien

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -71,12 +71,12 @@ defineProps({
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon" class="align-center justify-center object-scale-down p-1 ring-2 ring-yellow-300 dark:ring-gray-500 ">
             <p>{{ namePokemon }}</p>
         </div>
-        <div class="flex flex-col font-ketchum items-end mr-3 w-1/4 h-100vh">
+        <!-- <div class="flex flex-col font-ketchum items-end mr-3 w-1/4 h-100vh">
             <Link :href="route('event1')" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Event</Link>
             <Link :href="route('event2')" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Event</Link>
             <Link :href="route('event3')" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Event</Link>
 
-        </div>
+        </div> -->
     </div>
 
 </template>
--- resources/js/Pages/Team.vue
+++ resources/js/Pages/Team.vue
@@ -31,6 +31,6 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
             <div class="flex w-1/2 h-full justify-center object-contain">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon" class="align-center justify-center object-scale-down p-1 ring-2 ring-yellow-300 dark:ring-gray-500 ">
             <p>{{ namePokemon }}</p></div>
-        </div>
+    
     <p>team</p>
 </template>
