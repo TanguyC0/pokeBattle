446a9a4f817e335110653a41c0e3a21778c37e41
Elhya13 <aureliewinkin@gmail.com>
2023-01-23 14:20:34 +0100

    fix pick function

--- resources/js/Pages/Box.vue
+++ resources/js/Pages/Box.vue
@@ -6,22 +6,17 @@ const props = defineProps({
 });
 
 let nbSlot = props.listPokemon.length + (4 - (props.listPokemon.length%4));
-let pick = 0;
+// let pick = 0;
 // let grid = 'grid-cols-4 gap-4';
 // let list = 'grid-cols-1 gap-1';
-let name= props.listPokemon[pick].name;
-let type=  props.listPokemon[pick].type;
-let level = props.listPokemon[pick].level;
-let hp =  props.listPokemon[pick].hp;
-let xp = (props.listPokemon[pick].xp / props.listPokemon[pick].xpMax);
-let attack = props.listPokemon[pick].attack;
-let defense = props.listPokemon[pick].defense ; 
+
+
 </script>
 <script>
 export default {
     data() {
         return {
-            pick: 0,     
+            pick: 0   
         }
     },
 
@@ -58,10 +53,9 @@ export default {
                 </nav>
                 <ul class="grid grid-cols-4 gap-4 wrap bg-slate-300 w-full h-full">
                     <template v-for="n in nbSlot">
-                        <li :style="{}" class="m-1 w-full h-full  bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200" @click="pick = n-1">
+                        <li :style="{}" class="m-1 w-full h-full  bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200" @click=" pick= n-1">
                             <img v-if="n<=listPokemon.length" :src="`${listPokemon[n-1].image}`" alt="item" class="w-full justify-center">
                             
-                            <!-- <button @click="pick = n-1">click</button> -->
                         </li>
                     </template>
                 </ul>
@@ -69,12 +63,12 @@ export default {
             <section class="grid grid-cols-1 border rounded-lg shadow-lg bg-gray-600 border-gray-700 hover:bg-gray-700  ml-5 w-2/3 h-full
             ">
                 <h3 class="text-center text-white font-unown">Detail</h3>        
-                <p class="inline-flex items-center text-xl font-ketchum font-semibold  text-white hover:underline">Type: {{ type }}</p>
-                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Level: {{ level }}</p>
-                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Exp.: {{ xp }}</p>
-                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Hp: {{hp }}</p>
-                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Attack: {{ attack }}</p>
-                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Defense: {{ defense}}</p>
+                <p class="inline-flex items-center text-xl font-ketchum font-semibold  text-white hover:underline">Type: {{listPokemon[pick].type }}</p>
+                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Level: {{listPokemon[pick].level }}</p>
+                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Exp.: {{listPokemon[pick].xp}}</p>
+                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Hp: {{listPokemon[pick].hp}}</p>
+                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Attack: {{listPokemon[pick].attack}}</p>
+                <p class="inline-flex items-center text-xl font-normal text-white hover:underline">Defense: {{listPokemon[pick].defense}}</p>
             </section>
         </div>
     </main>
