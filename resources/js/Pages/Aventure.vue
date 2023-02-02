38179ce8f6f7e9ef7eedc5179e11122782c20598
Elhya13 <aureliewinkin@gmail.com>
2023-02-02 09:24:31 +0100

    adventure fix style

--- resources/js/Pages/Aventure.vue
+++ resources/js/Pages/Aventure.vue
@@ -37,7 +37,7 @@ const switchZone = (id) => {
 
     <div class ="w-full max-h-screen min-h-screen h-screen">
         <Header />
-        <main class="flex flex-row justify-around h-4/5 w-full ">
+        <main class="flex flex-row justify-around h-full w-full ">
 
             <section class="bg-red-600 bg-opacity-30 rounded-2xl border-4 p-5">
                 <h2 class="text-center text-5xl mb-10">TEAM</h2>
@@ -48,29 +48,29 @@ const switchZone = (id) => {
 
             
 
-            <section class="bg-red-600 bg-opacity-30 rounded-2xl border-4 w-2/5 p-5 flex flex-col">
+            <section class="bg-red-600 bg-opacity-30 rounded-2xl border-4 w-2/5 h-full p-5 flex flex-col">
                 <!-- carte -->
-                <div class="bg-[url('/img/map/map.jpg')] bg-no-repeat bg-contain h-3/5">
+                <div class="bg-[url('/img/map/map.jpg')] rounded bg-no-repeat bg-contain h-3/5">
                     <div v-for="zone in location" class="w-5 h-5 rounded-full border-2 relative z-10 bg-yellow-500" :class="inStage(zone, 'top-[@px] left-[@px]', stage), {'bg-pink-500' : active[zone.id-1]} " @click="stage = zone.id, switchZone(zone.id)"></div>
                 </div>
 
-                <section class="flex justify-center m-2">
-                    <div class="w-80">
-                        <p class="bg-white p-2">{{ message }}</p>
+                <section class="flex flex-wrap justify-center mt-2 h-2/5">
+                    <div class="w-60">
+                        <p class="bg-white p-2 rounded">{{ message }}</p>
                         <!-- bouton -->
-                        <div class="flex flex-row ">
-                            <Link :href="route('aventure.walk',`${stage}`)" class="w-36 h-20 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-3 my-2">walk</Link>
-                            <Link v-if="status == 2" :href="route('aventure.catch',[`${id}`,`${use}`])" class="w-36 h-20 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-3 my-2">catch</Link>
+                        <div class="flex flex-row justify-center ">
+                            <Link :href="route('aventure.walk',`${stage}`)" class="flex w-25 h-12 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-3 my-2">Walk</Link>
+                            <Link v-if="status == 2" :href="route('aventure.catch',[`${id}`,`${use}`])" class=" flex w-25 h-12 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-3 my-2">Catch</Link>
                         </div>
-                            <select v-if="status == 2" name="item" id="item" @change="use = $event.target.value" class="w-80 h-10 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 my-2">
+                            <select v-if="status == 2" name="item" id="item" @change="use = $event.target.value" class="flex w-50 h-10 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 my-2">
                                 <option v-for="pokeball in items" :key="pokeball.id" :value="pokeball.id">{{ pokeball.name }} ({{ pokeball.count }})</option>
                             </select>
+                        
                     </div>
-
                     <!-- vue -->
-                    <div class="flex flex-col">
-                        <div class="m-1 h-64 w-64  bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 text-center">
-                            <img :src="`${ img }`" alt="">
+                    <div class="flex flex-col justify-items-end">
+                        <div class="m-1 h-32 w-32 bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 text-center">
+                            <img :src="`${ img }`" alt="" class="flex">
                         </div>
                         
                     </div>
