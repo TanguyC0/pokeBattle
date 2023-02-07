6b135adc7fbf8352eed6057eaeaf7e74bd136604
TanguyC0 <corman.tanguy.ct@gmail.com>
2023-02-07 12:14:43 +0100

    start algo fight

--- app/Http/Controllers/AventureController.php
+++ app/Http/Controllers/AventureController.php
@@ -24,6 +24,455 @@ class AventureController extends Controller
             'location' => Stages::select('position','id')->get(),
             'team' => [],
         ];
+
+        $this->type_chart = [
+            'Normal' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 1,
+                'Rock' => 0.5,
+                'Bug' => 1,
+                'Ghost' => 0,
+                'Steel' => 0.5,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Fighting' => [
+                'Normal' => 2,
+                'Fighting' => 1,
+                'Flying' => 0.5,
+                'Poison' => 0.5,
+                'Ground' => 1,
+                'Rock' => 2,
+                'Bug' => 0.5,
+                'Ghost' => 0,
+                'Steel' => 2,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 0.5,
+                'Ice' => 2,
+                'Dragon' => 1,
+                'Dark' => 2,
+                'Fairy' => 0.5
+            ],
+            'Flying' => [
+                'Normal' => 1,
+                'Fighting' => 2,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 0,
+                'Rock' => 0.5,
+                'Bug' => 2,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 2,
+                'Electric' => 0.5,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Poison' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 1,
+                'Poison' => 0.5,
+                'Ground' => 0.5,
+                'Rock' => 0.5,
+                'Bug' => 1,
+                'Ghost' => 0.5,
+                'Steel' => 0,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 2,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 1,
+                'Fairy' => 2
+            ],
+            'Ground' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 0,
+                'Poison' => 2,
+                'Ground' => 1,
+                'Rock' => 2,
+                'Bug' => 0.5,
+                'Ghost' => 1,
+                'Steel' =>2,
+                'Fire' => 2,
+                'Water' => 1,
+                'Grass' => 0.5,
+                'Electric' => 2,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Rock' => [
+                'Normal' => 1,
+                'Fighting' => 0.5,
+                'Flying' => 2,
+                'Poison' => 1,
+                'Ground' => 0.5,
+                'Rock' => 1,
+                'Bug' => 2,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 2,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 2,
+                'Dragon' => 1,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Bug' => [
+                'Normal' => 1,
+                'Fighting' => 0.5,
+                'Flying' => 0.5,
+                'Poison' => 0.5,
+                'Ground' => 1,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 0.5,
+                'Steel' => 0.5,
+                'Fire' => 0.5,
+                'Water' => 1,
+                'Grass' => 2,
+                'Electric' => 1,
+                'Psychic' => 2,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 2,
+                'Fairy' => 0.5
+            ],
+            'Ghost' => [
+                'Normal' => 0,
+                'Fighting' => 1,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 1,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 2,
+                'Steel' => 1,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 2,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 0.5,
+                'Fairy' => 1
+            ],
+            'Steel' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 1,
+                'Rock' => 2,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 0.5,
+                'Water' => 0.5,
+                'Grass' => 1,
+                'Electric' => 0.5,
+                'Psychic' => 1,
+                'Ice' => 2,
+                'Dragon' => 1,
+                'Dark' => 1,
+                'Fairy' => 2
+            ],
+            'Fire' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying'=> 1,
+                'Poison' => 1,
+                'Ground' => 1,
+                'Rock' => 0.5,
+                'Bug' => 2,
+                'Ghost' => 1,
+                'Steel' => 2,
+                'Fire' => 0.5,
+                'Water' => 0.5,
+                'Grass' => 2,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 2,
+                'Dragon' => 0.5,
+                'Dark' => 1,
+                'Fairy' => 0.5
+            ],
+            'Water' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 2,
+                'Rock' => 2,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 1,
+                'Fire' => 2,
+                'Water' => 0.5,
+                'Grass' => 0.5,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 0.5,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Grass' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 0.5,
+                'Poison' => 0.5,
+                'Ground' => 2,
+                'Rock' => 2,
+                'Bug' => 0.5,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 0.5,
+                'Water' => 2,
+                'Grass' => 0.5,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 0.5,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Electric' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 2,
+                'Poison' => 1,
+                'Ground' => 0,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 1,
+                'Fire' => 1,
+                'Water' => 2,
+                'Grass' => 0.5,
+                'Electric' => 0.5,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 0.5,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Psychic' => [
+                'Normal' => 1,
+                'Fighting' => 2,
+                'Flying' => 1,
+                'Poison' => 2,
+                'Ground' => 1,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 0.5,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 0,
+                'Fairy' => 1
+            ],
+            'Ice' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 2,
+                'Poison' => 1,
+                'Ground' => 2,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 0.5,
+                'Water' => 0.5,
+                'Grass' => 2,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 0.5,
+                'Dragon' => 2,
+                'Dark' => 1,
+                'Fairy' => 1
+            ],
+            'Dragon' => [
+                'Normal' => 1,
+                'Fighting' => 1,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 1,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 2,
+                'Dark' => 1,
+                'Fairy' => 0
+            ],
+            'Dark' => [
+                'Normal' => 1,
+                'Fighting' => 0.5,
+                'Flying' => 1,
+                'Poison' => 1,
+                'Ground' => 1,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 2,
+                'Steel' => 1,
+                'Fire' => 1,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 2,
+                'Ice' => 1,
+                'Dragon' => 1,
+                'Dark' => 0.5,
+                'Fairy' => 0.5
+            ],
+            'Fairy' => [
+                'Normal' => 1,
+                'Fighting' => 2,
+                'Flying' => 1,
+                'Poison' => 0.5,
+                'Ground' => 1,
+                'Rock' => 1,
+                'Bug' => 1,
+                'Ghost' => 1,
+                'Steel' => 0.5,
+                'Fire' => 0.5,
+                'Water' => 1,
+                'Grass' => 1,
+                'Electric' => 1,
+                'Psychic' => 1,
+                'Ice' => 1,
+                'Dragon' => 2,
+                'Dark' => 2,
+                'Fairy' => 0.5
+            ],
+        ];
+        
+    }
+
+    //set message
+    public function setMessage($idUser,$num, $choix = 0, $itemList = [])
+    {
+        $listMessage = [
+            'Bienvenue dans l\'aventure', //0
+            'Vous n\'avez rien trouvé', //1
+            'Vous avez trouvé un coffre', //2
+            'Vous avez trouvé un pokemon', //3
+            'Vous avez attrapé le pokemon', //4
+            'Vous n\'avez pas attrapé le pokemon', //5
+            'Vous n\'avez pas de ball', //6
+            'vous n\'existe pas', //7
+            'Vous commencé un combat', //8
+            'Vous avez gagné le combat', //9
+            'Vous avez perdu le combat', //10
+            'vous n\'avez pas de pokemon', //11
+        ];
+
+        /*
+        Status :
+        0 = neutre / rien
+        1 = item
+        2 = pokemon trouvé
+        3 = apres catch, réussi ou non
+        4 = combat lancé
+        5 = apres combat, gagné ou perdu
+        */
+
+
+        switch($num)
+        {
+            case 0:
+            case 1:
+                $this->message['status'] = 0;
+                $this->message['img'] = '/img/'.['male','female'][rand(0,1)].'-character.png';
+                break;
+            case 2:
+                $this->message['status'] = 1;
+                $this->message['img'] = '../img/items/item'.$choix.'.png';
+
+                break;
+            case 3:
+                $this->message['status'] = 2;
+                $this->message['img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'.$choix.'.png';
+                break;
+            case 4:
+            case 5:
+                $this->message['status'] = 3;
+                $this->message['img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'.$choix.'.png';
+                break;
+            case 6:
+                $this->message['status'] = 3;
+                $this->message['img'] = '';
+                break;
+            case 7:
+                $this->message['status'] = 0;
+                $this->message['img'] = '/img/PokeGhost.png';
+                break;
+            case 8:
+                $this->message['status'] = 4;
+                $this->message['img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'.$choix.'.png';
+                break;
+            case 9:
+            case 10:
+                $this->message['status'] = 5;
+                $this->message['img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'.$choix.'.png';
+                break;
+            case 11:
+                $this->message['status'] = 0;
+                $this->message['img'] = '/img/PokeGhost.png';
+                break;
+        }
+
+        $this->message['message'] = $listMessage[$num];
+        $this->message['id'] = $choix;
+        $this->message['items'] = $itemList;
+
+        if ($idUser != 0)
+        {
+            $pokemon = new PokemonController();
+            $this->message['team'] = $pokemon->findPokemonUser(Box::where('id_user', $idUser)->whereIn('id', json_decode((User_game::where('id', $idUser)->get())[0]->team))->get());
+        }
+        
     }
 
     // for start the adventure
@@ -200,60 +649,89 @@ class AventureController extends Controller
         return $this->message;
     }
 
-    //set message
-    public function setMessage($idUser,$num, $choix = 0, $itemList = [])
+
+// in dev -------------------------------------------- fight
+    public function fight(Request $request)
     {
-        $listMessage = [
-            'Bienvenue dans l\'aventure', //0
-            'Vous n\'avez rien trouvé', //1
-            'Vous avez trouvé un coffre', //2
-            'Vous avez trouvé un pokemon', //3
-            'Vous avez attrapé le pokemon', //4
-            'Vous n\'avez pas attrapé le pokemon', //5
-            'Vous n\'avez pas de ball', //6
-            'vous n\'existe pas', //7
-        ];
-
-        switch($num)
+        $idUser = 0;
+        if($request->user())
         {
-            case 0:
-            case 1:
-                $this->message['status'] = 0;
-                $this->message['img'] = '/img/'.['male','female'][rand(0,1)].'-character.png';
-                break;
-            case 2:
-                $this->message['status'] = 1;
-                $this->message['img'] = '../img/items/item'.$choix.'.png';
+            $idUser = $request->user()->id;
 
-                break;
-            case 3:
-                $this->message['status'] = 2;
-                $this->message['img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'.$choix.'.png';
-                break;
-            case 4:
-            case 5:
-                $this->message['status'] = 3;
-                $this->message['img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'.$choix.'.png';
-                break;
-            case 6:
-                $this->message['status'] = 3;
-                $this->message['img'] = '';
-                break;
-            case 7:
-                $this->message['status'] = 0;
-                $this->message['img'] = '/img/PokeGhost.png';
-                break;
+            // get the team of the user_game
+            $team = User_game::select('team')->where('id_user', $idUser)->get()->toArray()[0]['team'];
+
+            if ($team != null) {
+                $id = json_decode($team, true);
+            }
+            else
+            {
+                $this->setMessage($idUser,11);
+                return $this->message;
+            }
+            
+            // get the team of the user
+            $userPokemon = Box::all()->whereIn('id', json_decode($team, true))
+                                    ->orderByRaw(DB::raw("field(id, ".implode(',', $id).")"))
+                                    ->get();
+
+            // get the pokemon of the enemy
+
+            $pv = 100;
+            $attack = 10;
+            $defense = 10;
+            $attack_type = 'normal';
+            $defend_type = ['normal'];
+
+            do {
+                $damage = $this->calculate_damage($attack, $defense, $this->calculateTypeModifier($attack_type, $defend_type));
+                $pv = $pv - $damage;
+                if ($pv > 0) {
+                    $damage = $this->calculate_damage($attack, $defense, $this->calculateTypeModifier($attack_type, $defend_type));
+                }
+            } while ($pv > 0);
+
+
+            
         }
-        
-        $this->message['message'] = $listMessage[$num];
-        $this->message['id'] = $choix;
-        $this->message['items'] = $itemList;
-
-        if ($idUser != 0)
+        else
         {
-            $pokemon = new PokemonController();
-            $this->message['team'] = $pokemon->findPokemonUser(Box::where('id_user', $idUser)->whereIn('id', json_decode((User_game::where('id', $idUser)->get())[0]->team))->get());
+            $this->setMessage($idUser,7);
         }
-        
+
+        return $this->message;
+    }
+
+    function calculateTypeModifier($attack_type, $defend_type) {
+    
+        $modifier = 0;
+        foreach ($defend_type as $value) {
+            $modifier += $this->type_chart[$attack_type][$value];
+        }
+        return $modifier / count($defend_type);
+    }
+
+    function calculate_damage($attack, $defense, $modifier) {
+
+        return ceil(((((2 * Niveau / 5 + 2) * $attack / $defense) / 50) + 2) * $modifier * random(0.85,1.15))
     }
 }
+
+
+
+
+/*
+fight rules
+
+DB :
+- list of pokemon
+- fourchette de niveau pokemon
+- fourchette de de pokemon
+- fourchette de de xp user
+
+fight :
+- verify if user have pokemon
+- verify if npc have pokemon
+- calculate degat (pokemon attack * type attack * random(0.85,1.15) - pokemon defense * type defense * random(0.85,1.15))
+
+*/
