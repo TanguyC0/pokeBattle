22bf43a50fc63446937d62982b4f60c26ea06f8f
TanguyC0 <corman.tanguy.ct@gmail.com>
2023-02-07 09:50:43 +0100

    add loading for walk

--- resources/js/Pages/Aventure.vue
+++ resources/js/Pages/Aventure.vue
@@ -30,6 +30,14 @@ const switchZone = (id) => {
 }
 
 function walk(){
+    message.value = {
+        message: 'loading...',
+        status: -1,
+        img: '/img/load/walk.gif',
+        location: message.value.location,
+        team: message.value.team,
+        items: [],
+    }
     const reponse = axios.post('/api/aventure/walk', {
         stage: stage.value,
     }).then((response) => {
@@ -38,6 +46,14 @@ function walk(){
 }
 
 function catchP(){
+    message.value = {
+        message: 'loading...',
+        status: -1,
+        img: '/img/load/pokeball.gif',
+        location: message.value.location,
+        team: message.value.team,
+        items: [],
+    }
     const reponse = axios.post('/api/aventure/catch', {
         id: message.value.id,
         idItem: use.value,
@@ -77,7 +93,7 @@ function catchP(){
                         <p class="bg-white p-2 rounded">{{ message.message }}</p>
 
                         <div class="flex flex-row justify-around ">
-                            <SmallButton color="red" @click="walk">Walk</SmallButton>
+                            <SmallButton v-if="message.status != -1" color="red" @click="walk">Walk</SmallButton>
                             <SmallButton v-if="message.status == 2" color="green" @click="catchP">Catch</SmallButton>
                             <!-- <Link :href="route('aventure.walk',`${stage}`)" class="flex w-25 h-12 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-3 my-2">Walk</Link> -->
                             <!-- <Link v-if="status == 2" :href="route('aventure.catch',[`${id}`,`${use}`])" class=" flex w-25 h-12 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-3 my-2">Catch</Link> -->
